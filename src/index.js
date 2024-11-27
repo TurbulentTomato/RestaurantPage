1dbdd0c00c9dd24338ed6c9515c9f96707e2ae51
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-11-27 12:23:07 +0530

    Add function to toggle Current Button

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ import "./base.css";
 
 const content = document.querySelector("#content");
 const buttons = Array.from(document.querySelectorAll("button"));
+
+buttons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    clearContent();
+    toggleCurrentButton(event.target)
+  })
+})
+
 buttons[0].addEventListener("click", () => {
-  clearContent();
   renderHome(content);
 });
 buttons[1].addEventListener("click", () => {
-  clearContent();
   renderMenu(content);
 });
 
 function clearContent() {
   content.innerHTML = "";
+}
+function toggleCurrentButton(target) {
+  buttons.map(button => {
+    if (Array.from(button.classList).includes("current-tab")) {
+      button.classList.toggle("current-tab");
+    }
+  })
+  target.classList.toggle("current-tab");
 }
