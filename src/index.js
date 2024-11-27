2577a8b6d6b9418b57f93309c8e3da2afeb187bd
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-11-27 11:16:10 +0530

    Add functionality to change tabs
    Clicking on a menu  button clears the current content and
    appends that tab's content to #content.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("hello hello hello")
+import { renderHome, renderMenu } from "./barrelModule.js";
+import "./reset.css";
+import "./base.css";
+
+const content = document.querySelector("#content");
+const buttons = Array.from(document.querySelectorAll("button"));
+buttons[0].addEventListener("click", () => {
+  clearContent();
+  renderHome(content);
+});
+buttons[1].addEventListener("click", () => {
+  clearContent();
+  renderMenu(content);
+});
+
+function clearContent() {
+  content.innerHTML = "";
+}
