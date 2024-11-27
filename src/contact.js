175b494236992e93f95d7eaa67fcdb9515f62537
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-11-27 21:57:07 +0530

    Add class "contact" to li

--- src/contact.js
+++ src/contact.js
@@ -5,8 +5,8 @@ export function renderContact(content) {
   contactHeading.textContent = "Contact us at:";
   const contactList = document.createElement("ul");
   contactList.innerHTML = `
-<li>+91 0000 0000 000</li>
-<li>totallyrealemail@totallynotfake.com</li>
+<li class="contact">+91 0000 0000 000</li>
+<li class="contact">totallyrealemail@totallynotfake.com</li>
 `;
   content.append(contactHeading, contactList);
 }
